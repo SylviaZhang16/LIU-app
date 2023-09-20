ea3cef0129dbb5ff4e4e73b52cf96d18a06e81c6
Sylvia Zhang <sylviaboom16@gmail.com>
2023-09-20 12:32:20 -0400

    basic components

---
+++ src/data/navData.js
@@ -0,0 +1,16 @@
+const navData = [
+    {
+      name: "Home",
+      path: "/home/",
+    },
+    {
+        name: "CheckIn",
+        path: "/check-in/",
+    },
+    {
+        name: "Profile",
+        path: "/profile/",
+    },
+  ]
+  
+export default navData;
